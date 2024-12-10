240664f5f60d2e2b4888cd39ab7a0b6b0107192c
cham24 <cameronchambers1215@gmail.com>
2024-12-09 21:26:06 -0700

    started rough draft for home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import ImageGallery from '../components/ImageGallery';
 
 function Home() {
     return (
@@ -14,13 +15,24 @@ function Home() {
                 </div>
 
                 <div className="content-section">
-                    {/* <p className="description">
-                        Mission Ink is looking to hire a new tattoo artist, and our door is always open to
-                        talented guest artists that want to come visit San Francisco. Please <a href="mailto:info@missionink.com">email</a> us
-                        with a brief bio and a link to your portfolio.
-                    </p> */}
-                    <h3 className="cta-title">Contact Us Now For Questions or to Schedule a Tattoo or Piercing</h3>
-                    <button className="cta-button">Contact Us Today!</button>
+                    <h3 className="cta-title">Contact Us Now For Questions or to Schedule a Tattoo or Piercing Consultation</h3>
+                    <a id="cta-button" href="./contact-us">Contact Us Today!</a>
+                </div>
+
+                <div className="gallery-section">
+                    <h2>Our Work</h2>
+                    <ImageGallery />
+                </div>
+
+                <div className="bottom-info">
+                    <div className="bottom-overlay">
+                        <p>
+                            Located in the heart of Cedar City, Mack's Tattoos and Piercing studio is the place to satisfy your craving for body art.
+                        </p>
+                        <p>
+                            We have a team of fun, artistic, and very talented artists. Check out our Yelp page and Reviews tab to see what our awesome clients have to say about us.
+                        </p>
+                    </div>
                 </div>
             </div>
         </div>
